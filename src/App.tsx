59876865057561b627c44c977d9f3688dae173b3
Abenezer <abitysank@gmail.com>
2026-01-30 11:31:27 -0500

    Router wiring and components
    Setup 3 pages, for home, login and register and wire them up with react router. Created some components files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
-function App() {
-
-    return <>Hello</>;
+export default function App() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App;
---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+    return (
+        <div>
+            <h1>Hello, its Register</h1>
+        </div>
+    );
+}
