c6fd9ee940813ab416536e09548d6ed62b214fd2
Abenezer <abitysank@gmail.com>
2026-01-30 13:21:38 -0500

    Bootstrap integration
    Installed bootstrap and add it to main.tsx for proper use

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./main.css";
 import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
