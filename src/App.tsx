fd0aab458b581ec6b42f5207d74db3a885687412
Abenezer <abitysank@gmail.com>
2026-02-01 10:16:04 -0500

    Update App.tsx
    Clean up main app using Bootstrap for width size and good things in place margin and spacing wise.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import { ThemeProvider } from "./context/Theme/ThemeProvider";
-import Navbar from "./components/Header/Header";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
 
 export default function App() {
     return (
         <>
             <ThemeProvider>
-                <div>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                    </Routes>
+                <div className="d-flex flex-column min-vh-100">
+                    <Header />
+                    <main className="flex-fill">
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route
+                                path="/register"
+                                element={<RegisterPage />}
+                            />
+                        </Routes>
+                    </main>
+
+                    <Footer />
                 </div>
             </ThemeProvider>
         </>
