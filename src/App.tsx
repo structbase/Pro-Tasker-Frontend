d1cc8bbc7468eb34ed613e2d09c8910ea56ea2d4
Abenezer <abitysank@gmail.com>
2026-01-31 21:17:58 -0500

    Update App.tsx
    Integrate themeProvider to the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { ThemeProvider } from "./context/Theme/ThemeProvider";
+import Navbar from "./components/Header/Header";
 
 export default function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-            </Routes>
+            <ThemeProvider>
+                <div>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                    </Routes>
+                </div>
+            </ThemeProvider>
         </>
     );
 }
-
