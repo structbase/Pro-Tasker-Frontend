5406b661c1a934160707c1e74af25d788a402d92
Abenezer <abitysank@gmail.com>
2026-02-01 17:27:57 -0500

    Update main.tsx
    wrap app with AuthProvider.

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import "./main.css";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { AuthProvider } from "./context/AuthContext/index.ts";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthProvider>
+                <App />
+            </AuthProvider>
         </BrowserRouter>
     </StrictMode>,
 );
