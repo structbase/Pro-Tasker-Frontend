7c0676eb9952c0557d548f91dd0471e191c4c4a1
Abenezer <abitysank@gmail.com>
2026-02-03 15:15:37 -0500

    Delete ThemeContext.tsx

--- src/context/Theme/ThemeContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import type { ThemeContextValue } from "../../types/theme-state";
-
-export const ThemeContext = createContext<ThemeContextValue | null>(null);
-
