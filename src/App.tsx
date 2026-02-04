ba2b3492788b175817b89b5bb16553e0ff137e4d
Abenezer <abitysank@gmail.com>
2026-02-04 11:21:20 -0500

    Refactor routes and add Edit page route
    Remove ThemeProvider wrapper and simplify App layout; move Header/Footer to top-level container and consolidate routing. Group public routes explicitly and replace individual ProtectedRoute wrappers with a nested Route element={<ProtectedRoute />} to protect multiple routes. Add EditPage import and new protected route /edit/:type/:id for editing, and clean up formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,41 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import { ThemeProvider } from "./context/Theme/ThemeProvider";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import ProjectsDashboard from "./pages/ProjectsDashboard/ProjectsDashboard";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage/ProjectDetailsPage";
+import EditPage from "./pages/EditPage/EditPage";
 
 export default function App() {
     return (
-        <>
-            <ThemeProvider>
-                <div className="d-flex flex-column min-vh-100">
-                    <Header />
-                    <main className="flex-fill">
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/login" element={<LoginPage />} />
-                            <Route
-                                path="/register"
-                                element={<RegisterPage />}
-                            />
+        <div className="d-flex flex-column min-vh-100">
+            <Header />
 
-                            <Route
-                                path="/projects"
-                                element={
-                                    <ProtectedRoute>
-                                        <ProjectsDashboard />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            <Route
-                                path="/projects/:projectId"
-                                element={
-                                    <ProtectedRoute>
-                                        <ProjectDetailsPage />
-                                    </ProtectedRoute>
-                                }
-                            />
-                        </Routes>
-                    </main>
+            <main className="flex-fill">
+                <Routes>
+                    {/* Public routes */}
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
 
-                    <Footer />
-                </div>
-            </ThemeProvider>
-        </>
+                    {/* Protected routes */}
+                    <Route element={<ProtectedRoute />}>
+                        <Route
+                            path="/projects"
+                            element={<ProjectsDashboard />}
+                        />
+                        <Route
+                            path="/projects/:projectId"
+                            element={<ProjectDetailsPage />}
+                        />
+                        <Route path="/edit/:type/:id" element={<EditPage />} />
+                    </Route>
+                </Routes>
+            </main>
+
+            <Footer />
+        </div>
     );
 }
