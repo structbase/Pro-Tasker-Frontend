09e2d484933bb85c191d4972e8cb9a03802f0816
Abenezer <abitysank@gmail.com>
2026-02-05 00:48:13 -0500

    Update axiosInstance.ts

--- src/utils/api/axiosInstance.ts
+++ src/utils/api/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios from "axios";
  * Uses a base URL and standard timeout for all API calls
  */
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     timeout: 8000,
     headers: {
         "Content-Type": "application/json",
