689d7198dd757fb4bc2c8def68ddc12847a58775
Abenezer <43732630+structbase@users.noreply.github.com>
2026-02-04 23:30:57 -0500

    Update Axios base URL for API calls

--- src/utils/api/axiosInstance.ts
+++ src/utils/api/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios from "axios";
  * Uses a base URL and standard timeout for all API calls
  */
 export const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api`,
+    baseURL: https://pro-tasker-backend-llnd.onrender.com/api,
     timeout: 8000,
     headers: {
         "Content-Type": "application/json",
